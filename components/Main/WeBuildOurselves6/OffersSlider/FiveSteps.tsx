502d102420a285cdcc0b34a2965594b342e2d23d
Dmitriy Vachugov <mitpal16@gmail.com>
2023-07-14 22:14:49 +0400

    fix 5 steps;

--- components/Main/WeBuildOurselves6/OffersSlider/FiveSteps.tsx
+++ components/Main/WeBuildOurselves6/OffersSlider/FiveSteps.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import listPoint from '@/public/icons/listPoint.svg';
+import React, { useEffect, useState } from 'react';
+import listPoint from '@/public/icons/listPoint1.svg';
 import Image from 'next/image';
-import { Fade } from 'react-awesome-reveal';
 import { useWindowSize } from '@/hooks/useWindowSize';
 
 const FiveSteps = () => {
@@ -9,12 +8,41 @@ const FiveSteps = () => {
 	// @ts-ignore
 	const isMobile = size.width <= 1439;
 
+	const [ step, setStep ] = useState<number>( 0 );
+	const [ scrolled, setScrolled ] = useState<boolean>( false );
+
+	const toShow = () => {
+		// @ts-ignore
+		setStep( count => count + 1 );
+	};
+
+	useEffect( () => {
+		const onScroll = () => {
+			// @ts-ignore
+			if ( document.documentElement.scrollTop > 4850 ) {
+				setScrolled( true );
+			} else {
+				setScrolled( false );
+			}
+		};
+
+		window.addEventListener( 'scroll', onScroll );
+
+		return () => {
+			window.removeEventListener( 'scroll', onScroll );
+		};
+	}, [] );
+
+	if ( scrolled ) {
+		setInterval( toShow, 1500 );
+	}
+
 	return (
 		<div className={ 'stepsWrapper container' }>
 
-			{/*{ isMobile*/}
-			{/*	?*/}
-			{/*	<>*/}
+			{ isMobile
+				?
+				<>
 					<div className={ 'step' }>
 						<h6 className={ 'text300 fz-80 pl-16' }>01</h6>
 						<Image src={ listPoint } alt={ '' }/>
@@ -60,66 +88,61 @@ const FiveSteps = () => {
 						</p>
 						<Image className={ 'lastImage' } src={ listPoint } alt={ '' }/>
 					</div>
-			{/*	</>*/}
-			{/*	:*/}
-			{/*	<div className={ 'container d-flex' }>*/}
-			{/*		<Fade delay={ 1e3 } cascade damping={ 1e-1 }>*/}
-			{/*			<div className={ 'step' }>*/}
-			{/*				<h6 className={ 'text300 fz-80 pl-16' }>01</h6>*/}
-			{/*				<Image src={ listPoint } alt={ '' }/>*/}
-			{/*				<div className={ 'stepLine' }/>*/}
-			{/*				<p className={ 'text300 colorWhite pl-16' }>*/}
-			{/*					Персонально подбираем недвижимость под ваши цели и бюджет*/}
-			{/*				</p>*/}
-			{/*			</div>*/}
-			{/*		</Fade>*/}
+				</>
+				:
+				<div className={ 'container' }>
+					<div className={ 'steps' }>
 
-			{/*		<Fade delay={ 2e3 } cascade damping={ 1e-1 }>*/}
-			{/*			<div className={ 'step' }>*/}
-			{/*				<h6 className={ 'text300 fz-80 pl-16' }>02</h6>*/}
-			{/*				<Image src={ listPoint } alt={ '' }/>*/}
-			{/*				<div className={ 'stepLine' }/>*/}
-			{/*				<p className={ 'text300 colorWhite pl-16' }>*/}
-			{/*					Рассчитываем доходность, знакомим с объектами и инфраструктурой*/}
-			{/*				</p>*/}
-			{/*			</div>*/}
-			{/*		</Fade>*/}
+						<div className={ 'step' } style={ step >= 1 ? { display: 'block' } : { display: 'none' } }>
+							<h6 className={ 'text300 fz-80 pl-16' }>01</h6>
+							<Image src={ listPoint } alt={ '' }/>
+							<div className={ 'stepLine' }/>
+							<p className={ 'text300 colorWhite pl-16' }>
+								Персонально подбираем недвижимость под ваши цели и бюджет
+							</p>
+						</div>
 
-			{/*		<Fade delay={ 3e3 } cascade damping={ 1e-1 }>*/}
-			{/*			<div className={ 'step' }>*/}
-			{/*				<h6 className={ 'text300 fz-80 pl-16' }>03</h6>*/}
-			{/*				<Image src={ listPoint } alt={ '' }/>*/}
-			{/*				<div className={ 'stepLine' }/>*/}
-			{/*				<p className={ 'text300 colorWhite pl-16' }>*/}
-			{/*					Договариваемся с застройщиком о выгодных для вас условиях и помогаем заключить сделку*/}
-			{/*				</p>*/}
-			{/*			</div>*/}
-			{/*		</Fade>*/}
+						<div className={ 'step' } style={ step >= 5 ? { display: 'block' } : { display: 'none' } }>
+							<h6 className={ 'text300 fz-80 pl-16' }>02</h6>
+							<Image src={ listPoint } alt={ '' }/>
+							<div className={ 'stepLine' }/>
+							<p className={ 'text300 colorWhite pl-16' }>
+								Рассчитываем доходность, знакомим с объектами и инфраструктурой
+							</p>
+						</div>
 
-			{/*		<Fade delay={ 4e3 } cascade damping={ 1e-1 }>*/}
-			{/*			<div className={ 'step' }>*/}
-			{/*				<h6 className={ 'text300 fz-80 pl-16' }>04</h6>*/}
-			{/*				<Image src={ listPoint } alt={ '' }/>*/}
-			{/*				<div className={ 'stepLine' }/>*/}
-			{/*				<p className={ 'text300 colorWhite pl-16' }>*/}
-			{/*					Юридически сопровождаем сделку: проверяем объект и помогаем с документами*/}
-			{/*				</p>*/}
-			{/*			</div>*/}
-			{/*		</Fade>*/}
+						<div className={ 'step' } style={ step >= 12 ? { display: 'block' } : { display: 'none' } }>
+							<h6 className={ 'text300 fz-80 pl-16' }>03</h6>
+							<Image src={ listPoint } alt={ '' }/>
+							<div className={ 'stepLine' }/>
+							<p className={ 'text300 colorWhite pl-16' }>
+								Договариваемся с застройщиком о выгодных для вас условиях и помогаем заключить сделку
+							</p>
+						</div>
 
-			{/*		<Fade delay={ 5e3 } cascade damping={ 1e-1 }>*/}
-			{/*			<div className={ 'step' }>*/}
-			{/*				<h6 className={ 'text300 fz-80 pl-16' }>05</h6>*/}
-			{/*				<Image src={ listPoint } alt={ '' }/>*/}
-			{/*				<div className={ 'stepLine' }/>*/}
-			{/*				<p className={ 'text300 colorWhite pl-16' }>*/}
-			{/*					Берём на себя управление недвижимостью на Пхукете и предоставляем услуги консьерж-сервиса*/}
-			{/*				</p>*/}
-			{/*				<Image className={ 'lastImage' } src={ listPoint } alt={ '' }/>*/}
-			{/*			</div>*/}
-			{/*		</Fade>*/}
-			{/*	</div>*/}
-			{/*}*/}
+						<div className={ 'step' } style={ step >= 20 ? { display: 'block' } : { display: 'none' } }>
+							<h6 className={ 'text300 fz-80 pl-16' }>04</h6>
+							<Image src={ listPoint } alt={ '' }/>
+							<div className={ 'stepLine' }/>
+							<p className={ 'text300 colorWhite pl-16' }>
+								Юридически сопровождаем сделку: проверяем объект и помогаем с документами
+							</p>
+						</div>
+
+						<div className={ 'step' } style={ step >= 33 ? { display: 'block' } : { display: 'none' } }>
+							<h6 className={ 'text300 fz-80 pl-16' }>05</h6>
+							<Image src={ listPoint } alt={ '' }/>
+							<div className={ 'stepLine' }/>
+							<p className={ 'text300 colorWhite pl-16' }>
+								Берём на себя управление недвижимостью на Пхукете и предоставляем услуги консьерж-сервиса
+							</p>
+							<Image className={ 'lastImage' } src={ listPoint } alt={ '' }/>
+						</div>
+					</div>
+
+					{/*<div className={ 'stepLine' }/>*/ }
+				</div>
+			}
 		</div>
 	);
 };
