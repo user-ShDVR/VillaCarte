b176e611a6a51bef3b776bcb60bca6092d0fa8b6
abramowsergey1992 <71379145+abramowsergey1992@users.noreply.github.com>
2023-08-04 13:50:33 +0500

    Правка шума (удали свои выводы .noise) + блюр

--- components/Main/ShareContent10/ShareContent10.tsx
+++ components/Main/ShareContent10/ShareContent10.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import YouTubeCard from '@/components/common/Cards/MediaCards/YouTubeCard/YouTubeCard';
-import DefaultMediaCard from '@/components/common/Cards/MediaCards/DefaultMediaCard';
-import youtubeIcon from '@/public/icons/socials/youtube.svg';
-import instagramIcon from '@/public/icons/socials/instagram.svg';
-import instaImg from '@/public/instaImg.png';
-import videoFrame_1 from '@/public/frameVideo_1.png';
-import videoFrame_2 from '@/public/frameVideo_2.png';
-import videoFrame_3 from '@/public/frameVideo_3.png';
-import ytMedia from '@/public/icons/socials/ytMedia.svg';
-import instaMedia from '@/public/icons/socials/instaMedia.svg';
-import { useStore } from 'effector-react';
-import { IMainPage } from '@/types/mainPage';
-import { $mainPageInfo } from '@/store/mainPage';
-import InstagramCard from '@/components/common/Cards/MediaCards/InstagramCard/InstagramCard';
-import { lang } from '@/pages';
-import noise from '@/public/noise.webp';
+import React from "react";
+import YouTubeCard from "@/components/common/Cards/MediaCards/YouTubeCard/YouTubeCard";
+import DefaultMediaCard from "@/components/common/Cards/MediaCards/DefaultMediaCard";
+import youtubeIcon from "@/public/icons/socials/youtube.svg";
+import instagramIcon from "@/public/icons/socials/instagram.svg";
+import instaImg from "@/public/instaImg.png";
+import videoFrame_1 from "@/public/frameVideo_1.png";
+import videoFrame_2 from "@/public/frameVideo_2.png";
+import videoFrame_3 from "@/public/frameVideo_3.png";
+import ytMedia from "@/public/icons/socials/ytMedia.svg";
+import instaMedia from "@/public/icons/socials/instaMedia.svg";
+import { useStore } from "effector-react";
+import { IMainPage } from "@/types/mainPage";
+import { $mainPageInfo } from "@/store/mainPage";
+import InstagramCard from "@/components/common/Cards/MediaCards/InstagramCard/InstagramCard";
+import { lang } from "@/pages";
+import noise from "@/public/noise.webp";
 
 const videos = [
-	{ image: videoFrame_1, path: 'https://youtu.be/ew2AIrxIdhM/', code: 1 },
-	{ image: videoFrame_2, path: 'https://youtu.be/Bwn1tmxIHFI/', code: 2 },
-	{ image: videoFrame_3, path: 'https://youtu.be/bpKGWdg5hoU/', code: 3 },
+	{ image: videoFrame_1, path: "https://youtu.be/ew2AIrxIdhM/", code: 1 },
+	{ image: videoFrame_2, path: "https://youtu.be/Bwn1tmxIHFI/", code: 2 },
+	{ image: videoFrame_3, path: "https://youtu.be/bpKGWdg5hoU/", code: 3 },
 	// { link: '9-WKpVg63tA' },
 	// { link: 'TR9qHODngPw' },
 	// { link: 'HB3a5_ffNGQ' },
@@ -29,52 +29,92 @@ const videos = [
 ];
 
 const photos = [
-	{ image: instaImg, path: 'https://www.instagram.com/p/Cu6uow9s2lk/', code: 1 },
-	{ image: instaImg, path: 'https://www.instagram.com/p/Cu6uow9s2lk/', code: 2 },
-	{ image: instaImg, path: 'https://www.instagram.com/p/Cu6uow9s2lk/', code: 3 },
-	{ image: instaImg, path: 'https://www.instagram.com/p/Cu6uow9s2lk/', code: 4 },
-	{ image: instaImg, path: 'https://www.instagram.com/p/Cu6uow9s2lk/', code: 5 },
-	{ image: instaImg, path: 'https://www.instagram.com/p/Cu6uow9s2lk/', code: 6 },
+	{
+		image: instaImg,
+		path: "https://www.instagram.com/p/Cu6uow9s2lk/",
+		code: 1,
+	},
+	{
+		image: instaImg,
+		path: "https://www.instagram.com/p/Cu6uow9s2lk/",
+		code: 2,
+	},
+	{
+		image: instaImg,
+		path: "https://www.instagram.com/p/Cu6uow9s2lk/",
+		code: 3,
+	},
+	{
+		image: instaImg,
+		path: "https://www.instagram.com/p/Cu6uow9s2lk/",
+		code: 4,
+	},
+	{
+		image: instaImg,
+		path: "https://www.instagram.com/p/Cu6uow9s2lk/",
+		code: 5,
+	},
+	{
+		image: instaImg,
+		path: "https://www.instagram.com/p/Cu6uow9s2lk/",
+		code: 6,
+	},
 ];
 
 const ShareContent = () => {
-	const media = useStore<IMainPage>( $mainPageInfo );
+	const media = useStore<IMainPage>($mainPageInfo);
 
 	return (
-		<div className={ 'shareContent' }>
-			{/*<div className={ 'bgOpacity' } style={ { backgroundImage: `url(${ palmsTransparent.src })`, opacity: .1 } }/>*/ }
-			<img src={ noise.src } alt="" className={ 'noise' }/>
+		<div className={"shareContent"}>
+			{/*<div className={ 'bgOpacity' } style={ { backgroundImage: `url(${ palmsTransparent.src })`, opacity: .1 } }/>*/}
+			{/* <img src={ noise.src } alt="" className={ 'noise' }/> */}
 
-			<div className={ 'container pt-60-80' }>
+			<div className={"container pt-60-80"}>
 				<div>
-					<h2 className={ 'h1Title ls--216' }>{ lang?.sharingExclusiveContentFromTheIsland || '' }</h2>
+					<h2 className={"h1Title ls--216"}>
+						{lang?.sharingExclusiveContentFromTheIsland || ""}
+					</h2>
 
-					<div className={ 'mediaCards' }>
-						<div className={ 'mediaCard-wrapper' }>
-							{ videos.slice( 0, 3 ).map( item =>
-								<YouTubeCard key={ item.code } image={ item.image.src } path={ item.path }/>
-							) }
+					<div className={"mediaCards"}>
+						<div className={"mediaCard-wrapper"}>
+							{videos.slice(0, 3).map((item) => (
+								<YouTubeCard
+									key={item.code}
+									image={item.image.src}
+									path={item.path}
+								/>
+							))}
 							<DefaultMediaCard
-								icon={ youtubeIcon }
-								hoveredIcon={ ytMedia }
-								text={ lang?.watchExpertReviews || '' }
-								link={ 'https://www.youtube.com/@VillaCartePhuket/videos' }
+								icon={youtubeIcon}
+								hoveredIcon={ytMedia}
+								text={lang?.watchExpertReviews || ""}
+								link={
+									"https://www.youtube.com/@VillaCartePhuket/videos"
+								}
 							/>
 						</div>
 
-						<div className={ 'mediaCard-wrapper photoCards' }>
-							{ photos.slice( 0, 5 ).map( item =>
-								<InstagramCard key={ item.code } image={ item.image.src } path={ item.path }/>
-							) }
+						<div className={"mediaCard-wrapper photoCards"}>
+							{photos.slice(0, 5).map((item) => (
+								<InstagramCard
+									key={item.code}
+									image={item.image.src}
+									path={item.path}
+								/>
+							))}
 							<DefaultMediaCard
-								icon={ instagramIcon }
-								hoveredIcon={ instaMedia }
-								text={ lang?.learnAllAboutRealEstateInThailand || '' }
-								link={ 'https://www.instagram.com/villacarte.vip/?igshid=NTc4MTIwNjQ2YQ%3D%3D' }
+								icon={instagramIcon}
+								hoveredIcon={instaMedia}
+								text={
+									lang?.learnAllAboutRealEstateInThailand ||
+									""
+								}
+								link={
+									"https://www.instagram.com/villacarte.vip/?igshid=NTc4MTIwNjQ2YQ%3D%3D"
+								}
 							/>
 						</div>
 					</div>
-
 				</div>
 			</div>
 		</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,35 @@
-import Head from 'next/head';
-import Layout from '../components/Layout/Layout';
-import React, { useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
-import { getMainPageInfo } from '@/store/mainPage';
-import { IMainPage } from '@/types/mainPage';
-import Image from 'next/image';
-import metaImage from '@/public/metaImageTest.png';
-import scrollArrow from '@/public/icons/scrollArrow.svg';
-import 'react-tabs/style/react-tabs.css';
-import weBuildBg from '@/public/weBuildBg.webp';
-import TopBlock from '@/components/Main/TopBlock';
-import AllServices from '@/components/Main/AllServices2';
-import BestProjects from '@/components/Main/BestProjects4';
-import JoinTheClub from '@/components/Main/JoinTheClub7';
-import VideoBlock from '@/components/Main/VideoBlock3/VideoBlock3';
-import FeedBackBlock from '@/components/Main/FeedBackBlock5';
-import WeAreTrusted from '@/components/Main/WeAreTrusted8/WeAreTrusted8';
-import WeCare from '@/components/Main/WeCare9';
-import WeBuildOurselves from '@/components/Main/WeBuildOurselves6/WeBuildOurselves6';
-import ShareContent from '@/components/Main/ShareContent10/ShareContent10';
-import Footer from '@/components/Main/Footer/Footer';
-import { enLocalization, ruLocalization } from '@/static/localization';
-import { useStore } from 'effector-react';
-import { $languages } from '@/store/languages';
-import Spinner from '@/components/common/Spinner/Spinner';
-import noise from '@/public/noise.webp';
+import Head from "next/head";
+import Layout from "../components/Layout/Layout";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { getMainPageInfo } from "@/store/mainPage";
+import { IMainPage } from "@/types/mainPage";
+import Image from "next/image";
+import metaImage from "@/public/metaImageTest.png";
+import scrollArrow from "@/public/icons/scrollArrow.svg";
+import "react-tabs/style/react-tabs.css";
+import weBuildBg from "@/public/weBuildBg.webp";
+import TopBlock from "@/components/Main/TopBlock";
+import AllServices from "@/components/Main/AllServices2";
+import BestProjects from "@/components/Main/BestProjects4";
+import JoinTheClub from "@/components/Main/JoinTheClub7";
+import VideoBlock from "@/components/Main/VideoBlock3/VideoBlock3";
+import FeedBackBlock from "@/components/Main/FeedBackBlock5";
+import WeAreTrusted from "@/components/Main/WeAreTrusted8/WeAreTrusted8";
+import WeCare from "@/components/Main/WeCare9";
+import WeBuildOurselves from "@/components/Main/WeBuildOurselves6/WeBuildOurselves6";
+import ShareContent from "@/components/Main/ShareContent10/ShareContent10";
+import Footer from "@/components/Main/Footer/Footer";
+import { enLocalization, ruLocalization } from "@/static/localization";
+import { useStore } from "effector-react";
+import { $languages } from "@/store/languages";
+import Spinner from "@/components/common/Spinner/Spinner";
+import noise from "@/public/noise.webp";
 async function getStaticProps() {
 	// @ts-ignore
-	const res = await fetch( `${ process.env.REACT_APP_URL }api/pages/thailand/index?lang=RU` );
+	const res = await fetch(
+		`${process.env.REACT_APP_URL}api/pages/thailand/index?lang=RU`
+	);
 	const info = await res.json();
 
 	return {
@@ -38,92 +40,111 @@ async function getStaticProps() {
 
 export let lang: any;
 
-export default function Home( { info }: any ) {
-	const languages = useStore( $languages );
-	switch ( languages.value ) {
-		case 'en':
+export default function Home({ info }: any) {
+	const languages = useStore($languages);
+	switch (languages.value) {
+		case "en":
 			lang = enLocalization;
 			break;
-		case 'ru':
+		case "ru":
 			lang = ruLocalization;
 			break;
 	}
 
-	const [ style, setStyle ] = useState<any>( { overflow: 'hidden' } );
+	const [style, setStyle] = useState<any>({ overflow: "hidden" });
 	// useWheelEvent();
 	// useWindowSize();
 
 	const handleClickScroll = () => {
-		const element = document.getElementById( 'section-2' );
-		if ( element ) {
-			element.scrollIntoView( { behavior: 'smooth' } );
+		const element = document.getElementById("section-2");
+		if (element) {
+			element.scrollIntoView({ behavior: "smooth" });
 		}
 	};
 
-	const {
-		isLoading,
-		data: mainPageInfo
-	} = useQuery<IMainPage>( [ 'mainPageInfo' ], getMainPageInfo );
+	const { isLoading, data: mainPageInfo } = useQuery<IMainPage>(
+		["mainPageInfo"],
+		getMainPageInfo
+	);
 
-	useEffect( () => {
-		setStyle( {} );
-	}, [] );
+	useEffect(() => {
+		setStyle({});
+	}, []);
 
 	return (
 		<>
-			{ isLoading ? <><Spinner/></>
-				:
+			{isLoading ? (
+				<>
+					<Spinner />
+				</>
+			) : (
 				<Layout>
 					<Head>
-						<meta property="og:title" content={ mainPageInfo?.meta.title }/>
-						<meta property="og:description" content={ mainPageInfo?.meta.description }/>
-						<meta property="og:keywords" content={ mainPageInfo?.meta.keywords }/>
-						<meta property="og:image" content={ metaImage.src }/>
+						<meta
+							property="og:title"
+							content={mainPageInfo?.meta.title}
+						/>
+						<meta
+							property="og:description"
+							content={mainPageInfo?.meta.description}
+						/>
+						<meta
+							property="og:keywords"
+							content={mainPageInfo?.meta.keywords}
+						/>
+						<meta property="og:image" content={metaImage.src} />
 					</Head>
 
-					<section className='pt-100-60 pos' style={ style } id="hero-section">
+					<section
+						className="pt-100-60 pos"
+						style={style}
+						id="hero-section">
+						<div className={"pos"}>
+							<TopBlock />
 
-						<div className={ 'pos' }>
-							<TopBlock/>
-
-							<button className="btn-scroll" onClick={ handleClickScroll }>
-								<Image src={ scrollArrow } alt={ 'scroll' }/>
+							<button
+								className="btn-scroll"
+								onClick={handleClickScroll}>
+								<Image src={scrollArrow} alt={"scroll"} />
 							</button>
 						</div>
 
 						<div id="section-2">
-							<AllServices/>
+							<AllServices />
 						</div>
 
-						<VideoBlock/>
+						<VideoBlock />
 
-						<BestProjects/>
+						<BestProjects />
 
-						<FeedBackBlock/>
+						<FeedBackBlock />
 
-						<div className={ 'pos overflow-hidden' }>
+						<div className={"pos overflow-hidden"}>
+							<div
+								style={{
+									backgroundImage: `url(${weBuildBg.src})`,
+								}}
+								className={"palmsTransparent-bg"}
+							/>
 
-							<div style={ { backgroundImage: `url(${ weBuildBg.src })` } } className={ 'palmsTransparent-bg' }/>
-
-							<WeBuildOurselves/>
+							<WeBuildOurselves />
 
 							<div>
-								<img src={ noise.src } alt="" className={ 'noise z-1 h-100-p-mobile' }/>
-							<JoinTheClub/>
+								{/* <img src={ noise.src } alt="" className={ 'noise z-1 h-100-p-mobile' }/> */}
+								<JoinTheClub />
 							</div>
 
-							<WeAreTrusted/>
+							<WeAreTrusted />
 
-							<WeCare/>
+							<WeCare />
 						</div>
 
-						<ShareContent/>
+						<ShareContent />
 
-						<Footer/>
+						<Footer />
 					</section>
 				</Layout>
-			}
+			)}
 		</>
 	);
 }
-
