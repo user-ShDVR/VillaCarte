4f1652a13cc878a8983c2433f70d5637a85ab79e
Dmitry O <theicesilver121@mail.ru>
2023-08-21 11:59:09 +0300

    fixed: error

--- components/query/index.tsx
+++ components/query/index.tsx
@@ -1,9 +1,15 @@
-'use client'
+'use client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query'; // Импортируем Hydrate
 import { useState } from 'react';
 
-export const QueryClientProviderWrapper: React.FC = ({ children }) => {
+interface QueryClientProviderWrapperProps {
+    children: React.ReactNode;
+}
+
+export const QueryClientProviderWrapper: React.FC<
+    QueryClientProviderWrapperProps
+> = ({ children }) => {
     // const queryClient = new QueryClient();
     const [queryClient] = useState(() => new QueryClient());
     return (
